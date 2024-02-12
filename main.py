6b2855822d12e425e44dfb90bc13f2aa2b297251
Sebastian Speetzen <sebastianspeetzen@googlemail.com>
2024-02-12 14:51:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 genai.configure(api_key="YOUR API KEY")
 model = genai.GenerativeModel('gemini-pro')
 
-def gemini(promt):
+def gemini(prompt):
     response = model.generate_content(contents=prompt)
     return response.text
 
